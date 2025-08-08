1a040422a8480cb9feb2f0de8e73152727884f54
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 15:44:04 +0000

    Fix settings loading logic and debounce delay default value handling

--- src-python/app/main.py
+++ src-python/app/main.py
@@ -7,13 +7,12 @@ import json
 
 main_dir = os.path.dirname(os.path.abspath(__file__))
 sound_path = os.path.join(main_dir, "sounds", "lizard_cleaned.wav")
-settings_file =  os.path.join(main_dir, "data", "settings.json")
+settings_file = os.path.join(main_dir, "data", "settings.json")
 
 sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
 
 last_played = 0
 
- 
 
 def on_press(key):
     print("key pressed {0}".format(key))
@@ -25,6 +24,7 @@ def on_press(key):
             return
             # sound_to_play_on_k_press.play()
         last_played = now
+
 
 # play_obj.wait_done() blocking not needed as for now..
 
@@ -40,8 +40,9 @@ def load_settings():
     else:
         return {"debounce_delay", 0.3}
 
+
 settings_data = load_settings()
-DEBOUNCE_DELAY = settings_data['debounce_delay'] | '0'
+DEBOUNCE_DELAY = settings_data.get("debounce_delay", 0.3)
 
 app = FastAPI()
 
