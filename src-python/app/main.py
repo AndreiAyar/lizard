1710f831f41b3421ffc907d67694ca5e2a73ff66
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 15:21:55 +0000

    Add debounce logic to prevent rapid sound playback on key press

--- src-python/app/main.py
+++ src-python/app/main.py
@@ -2,14 +2,26 @@ from fastapi import FastAPI
 from pynput import keyboard
 import simpleaudio as sa
 import os
+import time
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sound_path = os.path.join(script_dir, "sounds", "lizard.wav")
 sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
+last_played = 0
+DEBOUNCE_DELAY = 0.3  # seconds
+
+ 
 
 def on_press(key):
     print("key pressed {0}".format(key))
-    sound_to_play_on_k_press.play()
+
+    global last_played
+    now = time.time()
+    if now - last_played > DEBOUNCE_DELAY:
+        if sound_to_play_on_k_press:
+            sound_to_play_on_k_press.play()
+        last_played = now
+
 
 
 # play_obj.wait_done() blocking not needed as for now..
