4a2f885a175610c22b0b78b44075109c1c524318
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 19:46:58 +0000

    Update input value binding to use debounceValue instead of a static value

--- src-svelte/src/routes/+page.svelte
+++ src-svelte/src/routes/+page.svelte
@@ -30,5 +30,5 @@
 
 <h1>Welcome to SvelteKit</h1>
 
-<input type="range" min={0} max="100" value="40" on:change={onSliderChange} class="range" />
+<input type="range" min={0} max="100" value={debounceValue} onchange={onSliderChange} class="range" />
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
