3d789123cdeab33f7b73c15603c52b7a6eee555b
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 15:19:59 +0000

    Optimize sound object initialization by moving it outside the on_press function

--- src-python/app/main.py
+++ src-python/app/main.py
@@ -5,11 +5,10 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sound_path = os.path.join(script_dir, "sounds", "lizard.wav")
-
+sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
 
 def on_press(key):
     print("key pressed {0}".format(key))
-    sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
     sound_to_play_on_k_press.play()
 
 
