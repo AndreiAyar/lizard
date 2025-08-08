530a4b1bbabf6b173ecf2ebb4b737c7b7f6dcede
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 11:57:17 +0000

    Update key press logging to display full key object instead of character

--- src-python/app/main.py
+++ src-python/app/main.py
@@ -3,7 +3,8 @@ from pynput import keyboard
 
 
 def on_press(key):
-    print("key pressed {0}".format(key.char))
+    print("key pressed {0}".format(key))
+ 
 
 
 listener = keyboard.Listener(on_press=on_press)
