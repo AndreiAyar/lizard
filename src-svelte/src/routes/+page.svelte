4f156da990dbef83216a836ac48ac9eb0c7688de
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 19:41:51 +0000

    Add slider input with change event handler to log value changes

--- src-svelte/src/routes/+page.svelte
+++ src-svelte/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	function onSliderChange(event: Event) {
+		const value = (event.target as HTMLSelectElement).value;
+		console.log('val changed', value);
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+
+<input type="range" min={0} max="100" value="40" on:change={onSliderChange} class="range" />
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
