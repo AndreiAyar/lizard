c19c8dd89cb4ec0ce40196864916a17e46dd0bbf
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-08 15:17:43 +0000

    Fix sound object initialization inside the key press handler to ensure proper playback

--- src-python/app/main.py
+++ src-python/app/main.py
@@ -5,12 +5,12 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sound_path = os.path.join(script_dir, "sounds", "lizard.wav")
-sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
 
 
 def on_press(key):
     print("key pressed {0}".format(key))
-play_obj = sound_to_play_on_k_press.play()
+    sound_to_play_on_k_press = sa.WaveObject.from_wave_file(sound_path)
+    play_obj = sound_to_play_on_k_press.play()
 
 
 # play_obj.wait_done() blocking not needed as for now..
