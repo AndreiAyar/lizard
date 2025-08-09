53f322a9e13782ed016bdcda71df8992d02e6904
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-08-09 18:55:33 +0000

    Update checkbox to use custom styled toggle switch component

--- src-svelte/src/routes/+page.svelte
+++ src-svelte/src/routes/+page.svelte
@@ -59,7 +59,12 @@
 		<div class="text-center">
 			<h1 class="mb-2 text-6xl font-light text-gray-900">🦎</h1>
 			<h2 class="mb-1 text-3xl font-light text-gray-800">LIZARD</h2>
-           <input type="checkbox" defaultChecked class="toggle toggle-success" />
+			<label class=" inline-flex cursor-pointer items-center">
+				<input type="checkbox" class="peer sr-only" checked />
+				<div
+					class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-green-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-green-600 dark:peer-focus:ring-green-800"
+				></div>
+			</label>
 			<p class="text-sm text-gray-500">Keypress sound settings</p>
 		</div>
 
@@ -118,8 +123,6 @@
 </div>
 
 <style>
-
-
 	.slider::-webkit-slider-thumb {
 		appearance: none;
 		height: 20px;
